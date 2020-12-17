43d8bd8bbaa3767b26a1f792fba87f2bf52be033
Francesco <francesco.savignano96@gmail.com>
2020-12-17 17:16:03 +0100

    versione ajax impostata

--- dist/app.js
+++ dist/app.js
@@ -93,7 +93,32 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
+$(document).ready(function () {
+  var source = $("#card-hb-template").html();
+  var template = Handlebars.compile(source);
+  $.ajax({
+    url: '../database/dischi_json.php',
+    method: 'GET',
+    success: function success(data) {
+      console.log(data);
 
+      for (var i = 0; i < data.length; i++) {
+        var context = {
+          poster: data[i].poster,
+          title: data[i].title,
+          author: data[i].author,
+          genre: data[i].genre,
+          year: data[i].year
+        };
+        var card = template(context);
+        $('.cards-container').append(card);
+      }
+    },
+    error: function error() {
+      console.log('error');
+    }
+  });
+});
 
 /***/ }),
 
---
+++ src/app.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+    var source   = $("#card-hb-template").html();
+    var template = Handlebars.compile(source);
+
+    $.ajax({
+
+        url: '../database/dischi_json.php',
+        method: 'GET',
+        success:function(data){
+            console.log(data);
+
+            for (var i = 0; i < data.length; i++) {
+
+                var context = {
+                    poster: data[i].poster,
+                    title: data[i].title,
+                    author: data[i].author,
+                    genre: data[i].genre,
+                    year: data[i].year
+                };
+
+                var card = template(context);
+
+                $('.cards-container').append(card);
+
+            }
+
+        },
+        error:function(){
+            console.log('error');
+        },
+
+    });
+
+});
