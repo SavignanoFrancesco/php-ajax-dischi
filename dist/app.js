6da9b521a591b37298df612fd208dfedcf510cb8
Francesco <francesco.savignano96@gmail.com>
2020-12-17 18:17:42 +0100

    aggiunto filtro per genere tramite jquery

--- dist/app.js
+++ dist/app.js
@@ -118,7 +118,35 @@ $(document).ready(function () {
       console.log('error');
     }
   });
-  $("#select").change(function () {});
+  $("#select").change(function () {
+    var selected_genre = $("#select option:selected").text(); // alert(selected_genre);
+
+    $('.cards-container').empty();
+    $.ajax({
+      url: '../database/dischi_json.php',
+      method: 'GET',
+      success: function success(data) {
+        console.log(data);
+
+        for (var i = 0; i < data.length; i++) {
+          if (data[i].genre == selected_genre || selected_genre == 'All') {
+            var context = {
+              poster: data[i].poster,
+              title: data[i].title,
+              author: data[i].author,
+              genre: data[i].genre,
+              year: data[i].year
+            };
+            var card = template(context);
+            $('.cards-container').append(card);
+          }
+        }
+      },
+      error: function error() {
+        console.log('error');
+      }
+    });
+  });
 });
 
 /***/ }),
--- src/app.js
+++ src/app.js
@@ -34,7 +34,47 @@ $(document).ready(function(){
     });
 
     $( "#select" ).change(function() {
-        
+
+        var selected_genre = $( "#select option:selected" ).text();
+        // alert(selected_genre);
+
+        $('.cards-container').empty();
+
+        $.ajax({
+
+            url: '../database/dischi_json.php',
+            method: 'GET',
+            success:function(data){
+                console.log(data);
+
+                for (var i = 0; i < data.length; i++) {
+
+
+                    if (data[i].genre == selected_genre || selected_genre == 'All') {
+                        var context = {
+                            poster: data[i].poster,
+                            title: data[i].title,
+                            author: data[i].author,
+                            genre: data[i].genre,
+                            year: data[i].year
+                        };
+
+
+                        var card = template(context);
+
+                        $('.cards-container').append(card);
+
+                    }
+
+                }
+
+            },
+            error:function(){
+                console.log('error');
+            },
+
+        });
+
     });
 
 });
